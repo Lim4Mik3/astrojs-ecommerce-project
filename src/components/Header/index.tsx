5964e49186afc89874ca3d00ace462a0804e5469
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-05-09 17:00:35 -0300

    Create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,81 @@
-export default function Header() {
+import { memo } from "react";
+import { FaUserAlt, FaShoppingCart } from "react-icons/fa";
+
+interface MenuItem {
+  title: string;
+  path: string;
+}
+
+const MENU: MenuItem[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Shop",
+    path: "/shop",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+];
+
+function Header() {
   return (
-    <header className="flex align-middle justify-between h-24 bg-red-500 overflow-hidden px-20 py-1">
-      <div className="flex">
-        Flower
-        <p className="ml-1 flex">Shop</p>
+    <header className="flex items-center justify-between h-24 bg-white overflow-hidden px-20 py-1">
+      <div className="flex font-medium text-3xl">
+        <p className="text-orange-500">Flower</p>
+        <p className="ml-1">Shop</p>
       </div>
 
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Shop</li>
-          <li>Blog</li>
-          <li>About</li>
+      <nav className="flex items-center justify-between">
+        <ul className="flex list-none font-thin text-xl">
+          {MENU.map((menu, index) => {
+            const isFirstItem = index === 0;
+            const { title } = menu;
+
+            if (isFirstItem) {
+              return (
+                <li
+                  key={index}
+                  className={
+                    "transition ease-in-out duration-200 hover:text-orange-500 hover:cursor-pointer"
+                  }
+                >
+                  {title}
+                </li>
+              );
+            }
+
+            return (
+              <li
+                key={index}
+                className={
+                  "ml-6 transition ease-in-out duration-200 hover:text-orange-500 hover:cursor-pointer"
+                }
+              >
+                {title}
+              </li>
+            );
+          })}
         </ul>
       </nav>
+
+      <div className="flex items-center justify-center">
+        <a href="" className="mx-4">
+          <FaUserAlt className="text-orange-500" size={22} />
+        </a>
+        <a href="" className="mr-4">
+          <FaShoppingCart className="text-orange-500" size={22} />
+        </a>
+      </div>
     </header>
   );
 }
+
+export default memo(Header);
