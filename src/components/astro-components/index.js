cfc43faf4a69df8025dea31698f63fde27ea594b
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-05-11 05:57:43 -0300

    fixing default components exports and rename best selers component

--- src/components/astro-components/index.js
+++ src/components/astro-components/index.js
@@ -1,1 +1,3 @@
 export { default as HomeSection } from "./HomeSection.astro";
+export { default as BestSelersSection } from "./BestSelerSection.astro";
+export { default as LatestPostsSection } from "./LatestPostsSection.astro";
