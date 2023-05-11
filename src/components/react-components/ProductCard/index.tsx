a878a63225e4efe2f15f60d269da416df8a8d59c
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-05-11 05:52:09 -0300

    create best selers section home page and product card component

--- src/components/react-components/ProductCard/index.tsx
+++ src/components/react-components/ProductCard/index.tsx
@@ -1,3 +1,5 @@
+import { FaCartPlus } from "@react-icons/all-files/fa/FaCartPlus";
+
 interface Props {
   image: string;
   title: string;
@@ -6,19 +8,25 @@ interface Props {
 
 export default function ProductCard({ image, title, price }: Props) {
   return (
-    <div className="bg-white p-4 rounded-md">
+    <div className="bg-white p-4 rounded-md hover:cursor-pointer hover:scale-105 transition ease-in-out duration-200">
       <img
         src={image}
         alt={title}
         className="w-64 h-56 object-cover rounded-lg"
       />
 
-      <b>{title}</b>
+      <p className="mt-3 text-lg">{title}</p>
 
-      <footer>
-        <span>{price}</span>
+      <footer className="mt-6 flex items-center justify-between">
+        <span className="text-sm text-zinc-400 font-medium">{price}$</span>
 
-        <button type="button">Add to cart</button>
+        <button
+          type="button"
+          className="flex items-center justify-center text-sm text-orange-500 font-medium pl-3 pt-3 hover:text-orange-700 transition ease-in-out duration-200"
+        >
+          <FaCartPlus size={14} className="mr-2" />
+          Add to cart
+        </button>
       </footer>
     </div>
   );
