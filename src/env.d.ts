aa97ad544c93e1cd8f96bfc087943f5416c9bcd5
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-05-12 05:36:55 -0300

    Fix: alter the environment variables names to start with letter - required by vercel enviroment variable system

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly _CONTENT_SPACE_ID: string;
-  readonly _CONTENT_PUBLISHED_ACCESS_KEY: string;
-  readonly _CONTENT_PREVIEW_ACCESS_KEY: string;
+  readonly CONTENT_SPACE_ID: string;
+  readonly CONTENT_PUBLISHED_ACCESS_KEY: string;
+  readonly CONTENT_PREVIEW_ACCESS_KEY: string;
 }
--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,9 +1,9 @@
 import contentful from "contentful";
 
 export const contentfulClient = contentful.createClient({
-  space: import.meta.env._CONTENT_SPACE_ID,
+  space: import.meta.env.CONTENT_SPACE_ID,
   accessToken: import.meta.env.DEV
-    ? import.meta.env._CONTENT_PREVIEW_ACCESS_KEY
-    : import.meta.env._CONTENT_PUBLISHED_ACCESS_KEY,
+    ? import.meta.env.CONTENT_PREVIEW_ACCESS_KEY
+    : import.meta.env.CONTENT_PUBLISHED_ACCESS_KEY,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
